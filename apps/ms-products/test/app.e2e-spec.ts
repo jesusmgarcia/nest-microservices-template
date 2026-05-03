88667866b1b3e91418b4e1f172b90fa625ff008d
Jesus M. Garcia <jesusmgarciadev@gmail.com>
2026-05-03 15:14:26 +0200

    Project scaffold

---
+++ apps/ms-products/test/app.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { MsProductsModule } from '../src/app.module';
+
+describe('MsProductsController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [MsProductsModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
